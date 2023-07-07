37e889748fd4082fd9204274dc7ebaabc7cd06f4
judasaca <salamancajd14@gmail.com>
2023-07-07 18:16:46 -0500

    Add reactNode input to a component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
@@ -6,13 +7,7 @@ function App() {
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
-  return (
-    <ListGroup
-      items={items}
-      heading="Cities"
-      onSelectItem={handleSelectItem}
-    ></ListGroup>
-  );
+  return <Alert>This is an alert</Alert>;
 }
 
 export default App;

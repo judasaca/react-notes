723d879fda5a51e8c2214c66f206e6d5e652e40d
judasaca <salamancajd14@gmail.com>
2023-07-08 11:34:33 -0500

    Dinamic alert message with open/close button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
+import CustomButton from "./components/Button";
 
 function App() {
   const items = ["bogotá", "cali", "neiva", "manizales", "bucaramanga"];
+
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
-  return <Alert>This is an alert</Alert>;
+
+  const [display, changeDisplay] = useState(false);
+  return (
+    <>
+      {display && (
+        <Alert color="danger" onClose={() => changeDisplay(false)}>
+          This is an alert
+        </Alert>
+      )}
+      <CustomButton color="dark" onClick={() => changeDisplay(true)}>
+        Warning
+      </CustomButton>
+    </>
+  );
 }
 
 export default App;
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,10 +1,27 @@
 import { ReactNode } from "react";
 
 interface Props {
-  children: ReactNode;
+  children: string;
+  color: string;
+  onClose: () => void;
 }
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, color, onClose }: Props) => {
+  return (
+    <div
+      className={"alert alert-" + color + " alert-dismissible fade show"}
+      role="alert"
+    >
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+        // style="display:none"
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;

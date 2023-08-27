964156b909b1e0a5f520eab4fdc180a7b74323bf
judasaca2 <juandavid@vuedata.com>
2023-08-27 18:06:29 -0500

    Refactor.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-import { useState } from "react";
 import "./App.css";
-import Alert from "./components/Alert";
-import CustomButton from "./components/Button";
-import CustomListGroup from "./components/CustomListGroup";
-import styled from "styled-components";
-function App() {
-  const items = ["bogotá", "cali", "neiva", "manizales", "bucaramanga"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-  const Paragraph = styled.p`
-    background: blue;
-  `;
-  const [display, changeDisplay] = useState(false);
-  return (
-    <>
-      {display && (
-        <Alert color="danger" onClose={() => changeDisplay(false)}>
-          This is an alert
-        </Alert>
-      )}
-      <CustomButton color="dark" onClick={() => changeDisplay(true)}>
-        Warning
-      </CustomButton>
-      <CustomListGroup
-        items={items}
-        onSelectItem={handleSelectItem}
-        heading="LIST"
-      ></CustomListGroup>
-      <Paragraph>This is an styled component</Paragraph>
-    </>
-  );
+function App() {
+  return <div></div>;
 }
 
 export default App;
---
+++ src/components/OtherElements/Message.tsx
@@ -0,0 +1,6 @@
+let count = 0;
+const Message = () => {
+  count++;
+  return <div>Message {count}</div>;
+};
+export default Message;

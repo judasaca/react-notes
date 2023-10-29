cfa7bbc92d0c0cd8646f8092f03adec2385184ca
judasaca2 <juandavid@vuedata.com>
2023-10-29 18:25:51 -0500

    Added game cards with custom react hooks

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import rawApiClient from "../services/rawApiClient";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {
